7d54d420618e8a06d9314d66b37fc47ee40f7126
keraml <keraml@a69c751c-35fc-11de-9210-07d028069593>
2009-05-06 00:31:16 +0000

    uz to zacina byt funkcne aj bugove ;)

--- trunk/document_root/js/nette.js
+++ trunk/document_root/js/nette.js
@@ -44,54 +44,54 @@
  * @version    0.1
  */
 
-// jQuery.fn.extend({
-// 	ajaxSubmit: function (callback) {
-// 		var form;
-// 		var sendValues = {};
-// 	
-// 		// odesláno na tlačítku
-// 		if (this.is(":submit")) {
-// 			form = this.parents("form");
-// 			sendValues[this.attr("name")] = this.val() || "";
-// 	
-// 		// odesláno na formuláři
-// 		} else if (this.is("form")) {
-// 			form = this;
-// 	
-// 		// neplatný element, nic nedělat
-// 		} else {
-// 			return null;
-// 		}
-// 	
-// 		// validace
-// 		if (form.get(0).onsubmit && !form.get(0).onsubmit()) return;
-// 	
-// 		var values = form.serializeArray();
-// 	
-// 		for (var i = 0; i < values.length; i++) {
-// 			var name = values[i].name;
-// 	
-// 			// multi
-// 			if (name in sendValues) {
-// 				var val = sendValues[name];
-// 	
-// 				if (!(val instanceof Array)) {
-// 					val = [val];
-// 				}
-// 	
-// 				val.push(values[i].value);
-// 				sendValues[name] = val;
-// 			} else {
-// 				sendValues[name] = values[i].value;
-// 			}
-// 		}
-// 	
-// 		// odeslat ajaxový požadavek
-// 		return jQuery.ajax({
-// 			url: form.attr("action"),
-// 			data: sendValues,
-// 			type: form.attr("method") || "get",
-// 	    success: callback || null
-// 		});
-// 	}
-// });
+jQuery.fn.extend({
+	ajaxSubmit: function (callback) {
+		var form;
+		var sendValues = {};
+	
+		// odesláno na tlačítku
+		if (this.is(":submit")) {
+			form = this.parents("form");
+			sendValues[this.attr("name")] = this.val() || "";
+	
+		// odesláno na formuláři
+		} else if (this.is("form")) {
+			form = this;
+	
+		// neplatný element, nic nedělat
+		} else {
+			return null;
+		}
+	
+		// validace
+		if (form.get(0).onsubmit && !form.get(0).onsubmit()) return;
+	
+		var values = form.serializeArray();
+	
+		for (var i = 0; i < values.length; i++) {
+			var name = values[i].name;
+	
+			// multi
+			if (name in sendValues) {
+				var val = sendValues[name];
+	
+				if (!(val instanceof Array)) {
+					val = [val];
+				}
+	
+				val.push(values[i].value);
+				sendValues[name] = val;
+			} else {
+				sendValues[name] = values[i].value;
+			}
+		}
+	
+		// odeslat ajaxový požadavek
+		return jQuery.ajax({
+			url: form.attr("action"),
+			data: sendValues,
+			type: form.attr("method") || "get",
+	    success: callback || null
+		});
+	}
+});
